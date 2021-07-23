2347dadcf385190c9b6515d8ac269457b374d42b
IcmpOff <71145797+Icmp@users.noreply.github.com>
2021-07-24 00:32:04 +0100

    Update: npc-infinity

--- server-data/resources/[Core]/[NPC]/npc-infinity/client/cl_lib.lua
+++ server-data/resources/[Core]/[NPC]/npc-infinity/client/cl_lib.lua
@@ -1,19 +1,84 @@
-function IsPlayerActive(pServerId)
-    return exports['npc-infinity']:IsPlayerActive(pServerId)
+local Promises, PlayerCoords, EntityCoords = {}, {}, {}
+
+TriggerServerEvent("npc:infinity:player:ready")
+
+RegisterNetEvent('npc:infinity:player:coords')
+AddEventHandler('npc:infinity:player:coords', function (pCoords)
+    PlayerCoords = pCoords
+end)
+
+RegisterNetEvent('npc:infinity:entity:coords')
+AddEventHandler('npc:infinity:entity:coords', function (pNetId, pCoords)
+    if Promises[pNetId] then
+        Promises[pNetId]:resolve(pCoords)
+    end
+end)
+
+function FetchEntityCoords(pNetId)
+    local result = promise:new()
+    local timeout = PromiseTimeout(1000)
+
+    Promises[pNetId] = result
+
+    TriggerServerEvent('npc:infinity:entity:coords', pNetId)
+
+    local coords = Citizen.Await(promise.first({ timeout, result }))
+
+    EntityCoords[pNetId] = coords
+
+    Citizen.SetTimeout(1000, function()
+        EntityCoords[pNetId] = nil
+    end)
+
+    Promises[pNetId] = nil
+
+    return coords
 end
 
-function DoesPlayerExist(pServerId)
-    return exports['npc-infinity']:DoesPlayerExist(pServerId)
-end
+exports('GetPlayerCoords', function (pServerId)
+    return PlayerCoords[tonumber(pServerId)]
+end)
 
-function GetPlayerCoords(pServerId)
-    return exports['npc-infinity']:GetPlayerCoords(pServerId)
-end
+exports('DoesPlayerExist', function (pServerId)
+    local playerId = GetPlayerFromServerId(tonumber(pServerId))
 
-function GetNetworkedCoords(pType, pNetId)
-    return exports['npc-infinity']:GetNetworkedCoords(pType, pNetId)
-end
+    if playerId ~= -1 then
+        return true
+    end
+end)
 
-function GetLocalEntity(pType, pNetId)
-    return exports['npc-infinity']:GetLocalEntity(pType, pNetId)
-end
+exports('IsPlayerActive', function (pServerId)
+    return PlayerCoords[tonumber(pServerId)] ~= nil
+end)
+
+exports('GetNetworkedCoords', function (pType, pNetId)
+    local coords
+
+    if pType == 'player' then
+        local playerIndex = GetPlayerFromServerId(pNetId)
+        coords = playerIndex == -1 and PlayerCoords[pNetId] or GetEntityCoords(GetPlayerPed(playerIndex))
+    else
+        local entity = NetworkGetEntityFromNetworkId(pNetId)
+
+        if DoesEntityExist(entity) then
+            coords = GetEntityCoords(entity)
+        else
+            coords = EntityCoords[pNetId] or FetchEntityCoords(pNetId)
+        end
+    end
+
+    return coords
+end)
+
+exports('GetLocalEntity', function (pType, pNetId)
+    local entity
+
+    if pType == 'player' then
+        local playerIndex = GetPlayerFromServerId(pNetId)
+        entity = playerIndex ~= -1 and GetPlayerPed(playerIndex) or 0
+    else
+        entity = NetworkGetEntityFromNetworkId(pNetId)
+    end
+
+    return entity
+end)
--- server-data/resources/[Core]/[NPC]/npc-infinity/server/sv_lib.lua
+++ server-data/resources/[Core]/[NPC]/npc-infinity/server/sv_lib.lua
@@ -1,17 +1,84 @@
 RegisterServerEvent('npc:infinity:player:ready')
 AddEventHandler('npc:infinity:player:ready', function()
-    local ting = GetEntityCoords(GetPlayerPed(source))
-    
-    TriggerClientEvent('npc:infinity:player:coords', -1, ting)
+    local sexinthetube = GetEntityCoords(GetPlayerPed(source)) 
+    TriggerClientEvent('npc:infinity:player:coords', -1, sexinthetube)
 end)
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(30000)
-        local ting = GetEntityCoords(source)
+        local sexinthetube = GetEntityCoords(source)
 
-        TriggerClientEvent('npc:infinity:player:coords', -1, ting)
-        TriggerEvent("npc-core:updatecoords", ting.x, ting.y, ting.z)
+        TriggerClientEvent('npc:infinity:player:coords', -1, sexinthetube)
+        TriggerEvent("npc-core:updatecoords", sexinthetube.x, sexinthetube.y, sexinthetube.z)
        -- print("[^2npc-infinity^0]^3 Sync Successful.^0")
     end
+end)
+
+RegisterServerEvent('player:setJob')
+AddEventHandler('player:setJob', function(cid, job)
+
+    local src = source
+
+    exports.ghmattimysql:execute('UPDATE __characters SET job= ? WHERE id= ?', {job, cid})
+end)
+
+RegisterServerEvent('player:setRank')
+AddEventHandler('player:setRank', function(cid, rank)
+
+    local src = source
+
+    exports.ghmattimysql:execute("UPDATE __characters SET rank = @rank WHERE id = @id", { 
+        ['@id'] = cid,
+        ['@rank'] = tonumber(rank)
+    })
+    -- exports.ghmattimysql:execute('UPDATE __characters SET rank= ? WHERE `id`= ?', {tonumber(rank), cid})
+end)
+
+RegisterServerEvent('player:removeCash')
+AddEventHandler('player:removeCash', function(cid, amount)
+
+    local src = source
+
+    exports.ghmattimysql:execute('UPDATE __characters SET cash= ? WHERE id= ?', {amount, cid})
+end)
+
+RegisterServerEvent('player:addCash')
+AddEventHandler('player:addCash', function(cid, amount)
+
+    local src = source
+
+    exports.ghmattimysql:execute('UPDATE __characters SET cash= ? WHERE id= ?', {amount, cid})
+end)
+
+RegisterServerEvent('player:setCash')
+AddEventHandler('player:setCash', function(cid, amount)
+
+    local src = source
+
+    exports.ghmattimysql:execute('UPDATE __characters SET cash= ? WHERE id= ?', {amount, cid})
+end)
+
+RegisterServerEvent('player:removeBank')
+AddEventHandler('player:removeBank', function(cid, amount)
+
+    local src = source
+
+    exports.ghmattimysql:execute('UPDATE __characters SET bank= ? WHERE id= ?', {amount, cid})
+end)
+
+RegisterServerEvent('player:addBank')
+AddEventHandler('player:addBank', function(cid, amount)
+
+    local src = source
+
+    exports.ghmattimysql:execute('UPDATE __characters SET bank= ? WHERE id= ?', {amount, cid})
+end)
+
+RegisterServerEvent('player:setBank')
+AddEventHandler('player:setBank', function(cid, amount)
+
+    local src = source
+
+    exports.ghmattimysql:execute('UPDATE __characters SET bank= ? WHERE id= ?', {amount, cid})
 end)
